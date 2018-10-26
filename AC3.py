f15393aa9cda8e956b48a77a4fdf304e9a95bc15
wuwica <wuwicajon@gmail.com>
2018-10-26 17:10:41 -0400

    Backtracking + stack and a few other minor things

--- AC3.py
+++ AC3.py
@@ -1,15 +1,27 @@
 def AC3(domains,unsolved): 
-  unsolved.sort(key=len)
+  unsolved.sort(key=len(domains[unsolved[0]][unsolved[1]]),reverse = True) #idk if this works but check changes done to sudoku aswell
   while not unfinished:
     current=finished.pop(0)
-    if len(current[0])==1:
-      apply_constraints(domains,current)
-    else: #Start pulling arbitrary values and backtrack if doesnt work out.
+    if len(domains[current[0]][current[1]])==1:
+      if not apply_constraints(domains,current):
+        revert = stateStack.pop(0)
+        domains = revert.domains
+        unsolved = revert.unsolved
+        unfinished.append(current)
+    else:
+      possibilities = domains[current[0]][current[1]].copy
+      for x in possibilities:
+        domains[current[0]][current[1]] = [x]
+        state = State(domains.copy,unsolved.copy)
+        stateStack.append(state)
+      unfinished.append(current)
+
+
 
 def apply_constraints(domains,current):
     n=9
-    x=current[1]
-    y=current[2]
+    x=current[0]
+    y=current[1]
 
     box_x=x//3
     rel_x=x%3
@@ -22,9 +34,22 @@ def apply_constraints(domains,current):
     
     for n in setx:
       for m in sety:
-        domains[box_y*3+m][box_x*3+n].remove(current[0])
+        domains[box_y*3+m][box_x*3+n].remove(domains[current[0]][current[1]])
+        if (not domains[box_y*3+m][box_x*3+n]):
+          return false
+
     for i in range(n-1):
       if not i==y:
-        domains[i][x].remove(current[0]) 
+        domains[i][x].remove(domains[current[0]][current[1]]) 
+        if (not domains[i][x]):
+          return false
       if not i==x:
-        domains[y][i].remove(current[0])
+        domains[y][i].remove(domains[current[0]][current[1]])
+        if (not domains[y][i]):
+          return false
+    return true
+
+class State:
+  def __init__(domains, unsolved):
+    self.domains = domains
+    self.unsolved = unsolved
--- sudoku.py
+++ sudoku.py
@@ -15,7 +15,7 @@ domains=[None]*9
 for x in range(n):
   domains[x]=[None]*9
   for y in range(n):
-    domains[x][y]=[0,1,2,3,4,5,6,7,8,9]
+    domains[x][y]=[1,2,3,4,5,6,7,8,9]
 
 for x in range(n):
   print(sudoku[x])
@@ -65,12 +65,13 @@ for i in range(n):
     if sudoku[i][j]!=None:
 #      print(domains[i][j],i,j)
       domains[i][j]=[sudoku[i][j]]
-    
-    unsolved.append([sudoku[i][j],i,j])
+    unsolved.append([i,j])
 #  print(domains[i], "indexed domain out of loop",i)
    
 
 for x in range(n):
   print(domains[x])
 
-
+print" "
+for val in unsolved:
+  print val
